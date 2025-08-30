957d19880830ef54fe0bffdb7efbc5ae333e4937
mahaliabpqvn07 <mahaliabpqvn07@gmail.com>
2025-08-30 16:24:38 +0000

    Add files via upload

---
+++ pre-install-uam.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+sudo apt update
+sudo apt install nload && sudo apt install docker.io -y && sudo sed -ie 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1'/g /etc/sysctl.conf && sudo sysctl --system && sudo apt install docker-compose -y
+sudo chmod 666 /var/run/docker.sock
+net=$1
+sudo iptables -t nat -I POSTROUTING -s 172.17.0.1 -j SNAT --to-source $(ip addr show $net | grep "inet " | grep -v 127.0.0.1|awk 'match($0, /(10.[0-9]+\.[0-9]+\.[0-9]+)/) {print substr($0,RSTART,RLENGTH)}')
+sudo apt purge ntp -y
+sudo systemctl start systemd-timesyncd
+sudo systemctl status systemd-timesyncd >null
+cat null
+echo "DONE"
