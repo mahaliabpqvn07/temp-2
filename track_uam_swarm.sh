c1008fcf79b04be9687fb06a8835b761df5812b3
mahaliabpqvn07 <mahaliabpqvn07@gmail.com>
2026-01-23 12:21:46 +0700

    Update track_uam_swarm.sh

--- track_uam_swarm.sh
+++ track_uam_swarm.sh
@@ -278,14 +278,14 @@ echo "Total Threads: $totalThreads"
 #    setNewThreadUAM=1
 #fi
 
-if [[ $cpu_cores -eq 48 && $totalThreads -gt 12 ]]; then
-    docker rm -f uam_13 uam_14
-    totalThreads=12
+if [[ $cpu_cores -eq 48 && $totalThreads -gt 10 ]]; then
+    docker rm -f uam_13 uam_14 uam_12 uam_11
+    totalThreads=10
 fi
 
-if [[ $cpu_cores -eq 256 && $totalThreads -gt 52 ]]; then
-    docker rm -f uam_55 uam_54 uam_53
-    totalThreads=52
+if [[ $cpu_cores -eq 256 && $totalThreads -gt 50 ]]; then
+    docker rm -f uam_55 uam_54 uam_53 uam_52 uam_51
+    totalThreads=50
 fi
 
 if [ "$setNewThreadUAM" -gt 0 ]; then
