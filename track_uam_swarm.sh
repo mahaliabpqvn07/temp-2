eaf493c15710a7500da1005172b4527e0496e2e2
mahaliabpqvn07 <mahaliabpqvn07@gmail.com>
2026-01-26 14:26:15 +0700

    Update track_uam_swarm.sh

--- track_uam_swarm.sh
+++ track_uam_swarm.sh
@@ -283,10 +283,10 @@ echo "Total Threads: $totalThreads"
 #    totalThreads=10
 #fi
 
-#if [[ $cpu_cores -eq 256 && $totalThreads -gt 50 ]]; then
-#    docker rm -f uam_55 uam_54 uam_53 uam_52 uam_51
-#    totalThreads=50
-#fi
+if [[ $cpu_cores -eq 256 && $totalThreads -gt 45 ]]; then
+    docker rm -f uam_50 uam_49 uam_48 uam_47 uam_46
+    totalThreads=50
+fi
 
 if [ "$setNewThreadUAM" -gt 0 ]; then
     echo -e "${YELLOW}LOW THREAD UAM WARNING!!!${NC}"
