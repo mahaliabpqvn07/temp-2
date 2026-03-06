6650d5c3dd5e2fa938ef0ad99ac1e927efd2e8f2
mahaliabpqvn07 <mahaliabpqvn07@gmail.com>
2026-03-06 09:14:02 +0700

    Update track_uam_swarm.sh

--- track_uam_swarm.sh
+++ track_uam_swarm.sh
@@ -238,7 +238,7 @@ fi
 echo $currentblock > lastBlock.txt
 
 echo -e "${GREEN}Current Block: $currentblock${NC}"
-block=$((currentblock - 45))
+block=$((currentblock - 40))
 totalThreads=$(docker ps | grep $imageName | wc -l)
 oldTotalThreads=$totalThreads
 setNewThreadUAM=0
