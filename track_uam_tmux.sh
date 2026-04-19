1dbea8c47390b353e5ce49d79265e8f5c14ffac2
mahaliabpqvn07 <mahaliabpqvn07@gmail.com>
2026-04-19 17:41:38 +0700

    Update track_uam_tmux.sh

--- track_uam_tmux.sh
+++ track_uam_tmux.sh
@@ -247,8 +247,17 @@ install_uam() {
     echo -e "${GREEN}Installed ${total_threads} threads successfully!${NC}"
 }
 
+if [ "$setNewThreadUAM" -gt 0 ]; then
+    echo -e "${YELLOW}NO THREAD UAM WARNING!!!${NC}"
+    echo -e "${YELLOW}Starting new thread...${NC}"
+fi
+
 if [ "$setNewThreadUAM" -gt 0 ] || [ ${#restarted_threads[@]} -gt 0 ]; then
     install_uam $PBKEY
+fi
+
+if [ "$setNewThreadUAM" -gt 0 ]; then
+    send_telegram_notification "$nowDate%0A%0A ⚠️ UAM NEW THREAD ALERT!!!%0A%0AIP: $PUBLIC_IP%0AISP: $ISP%0AOrg: $ORG%0ACountry: $COUNTRY%0ARegion: $REGION%0ACity: $CITY%0A%0A✅ System Information:%0A----------------------------%0AOS: $os_name%0ATotal CPU Cores: $cpu_cores%0ACPU Name: $cpu_name%0ACPU Load: $cpu_load%%0ATotal RAM: $total_ram MB%0ARAM Usage: $ram_usage%%0AAvailable RAM: $available_ram MB%0ADisk Usage (Root): $disk_usage%0AUptime: $uptime%0A%0A✅ UAM Information:%0A----------------------------%0APBKey: $PBKEY%0AImage: $imageName%0A%0ASNew Threads: 1"
 fi
 
 if [ ${#restarted_threads[@]} -gt 0 ]; then
