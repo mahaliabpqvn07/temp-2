7a40f113d4010c061d6faf59a74bade186a4c9ef
mahaliabpqvn07 <mahaliabpqvn07@gmail.com>
2026-05-02 20:56:18 +0700

    Update ovh-pre-install-uam.sh

--- ovh-pre-install-uam.sh
+++ ovh-pre-install-uam.sh
@@ -6,7 +6,17 @@ sudo iptables -F
 sudo iptables -A INPUT -p all -j ACCEPT
 sudo iptables -A FORWARD -p all -j ACCEPT
 sudo iptables -A OUTPUT -p all -j ACCEPT
-sudo iptables -t nat -I POSTROUTING -s 172.17.0.1 -j SNAT --to-source $(ip addr show ens3 | grep "inet " | grep -v 127.0.0.1|awk 'match($0, /([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)/) {print substr($0,RSTART,RLENGTH)}')
+# Get the first non-loopback interface
+net=$(ip link show | awk -F: '/^[0-9]+:/ {print $2}' | tr -d ' ' | grep -v '^lo$' | head -n1)
+
+if [[ -z "$net" ]]; then
+    echo "No network interface found."
+    exit 1
+else
+    echo "First network interface: $net"
+fi
+
+sudo iptables -t nat -I POSTROUTING -s 172.17.0.1 -j SNAT --to-source $(ip addr show $net | grep "inet " | grep -v 127.0.0.1|awk 'match($0, /([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)/) {print substr($0,RSTART,RLENGTH)}')
 sudo apt purge ntp -y
 sudo systemctl start systemd-timesyncd
 sudo systemctl status systemd-timesyncd >>null
