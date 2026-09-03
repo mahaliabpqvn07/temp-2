88eb89bb26373185f72a168f48fe37222c3d1e26
mahaliabpqvn07 <mahaliabpqvn07@gmail.com>
2026-09-03 20:44:15 +0700

    Update track_uam_swarm.sh

--- track_uam_swarm.sh
+++ track_uam_swarm.sh
@@ -175,7 +175,7 @@ get_current_block_self() {
         fromBlock=184846
     fi
     while [ $retry_count -lt $max_retries ]; do
-        currentblock=$(curl -s -X POST http://134.185.80.127:22825/api/1.0 \
+        currentblock=$(curl -s -X POST http://157.230.45.47:22825/api/1.0 \
             -H "Content-Type: application/json" \
             -d '{
                 "method": "getMiningBlocksWithTreasury",
